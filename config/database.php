449736a4a95bcc2f06a31b1a84898c4cbd8e1f5c
smashdog <smashdog@qq.com>
2019-05-07 19:06:17 +0800

    数据库基础配置

--- config/database.php
+++ config/database.php
@@ -1,4 +1,12 @@
 <?php
 
 return [
+    'driver' => 'mysql',
+    'host' => '',
+    'database' => '',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
 ];
