c2e2feb23db0b6c41b3144cb6f04015dc61b71a5
smashdog <smashdog@qq.com>
2019-05-09 14:38:37 +0800

    默认配置文件

--- config/app.php
+++ config/app.php
@@ -1,9 +1,15 @@
 <?php
 
 return [
-    'swoole' => [
-        'http' => [
-            'port' => 9501,
-        ],
+    //默认项目根目录
+    'app_path' => 'app',
+    //命名空间默认路径
+    'pathlist' => [
+        'controller',
+        'logic',
+        'model',
+        'command',
     ],
+    //默认路由
+    'default_route' => 'index/index/index',
 ];
